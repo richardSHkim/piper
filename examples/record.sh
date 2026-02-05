f48389e0ac76ccf0d65adc91d3defd325afcbe1c
richardSHkim <shkim@superb-ai.com>
2026-02-05 06:56:49 +0000

    add two cameras

--- examples/record.sh
+++ examples/record.sh
@@ -5,6 +5,7 @@ lerobot-record \
     --robot.type=piper_follower \
     --robot.can_name=can_follower \
     --robot.id=piper_follower_01 \
+    --robot.cameras="{ rgb: {type: realsense, serial_number_or_name: '323622272575', width: 848, height: 480, fps: 30}, fisheye: {type: opencv, index_or_path: 8, width: 640, height: 480, fps: 30}}" \
     --teleop.type=piper_leader \
     --teleop.can_name=can_leader \
     --teleop.id=piper_leader_01 \
