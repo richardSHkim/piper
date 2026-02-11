0eac6c2b0b6b3c493f3acd006f41bb69bbbb9ba4
richardSHkim <shkim@superb-ai.com>
2026-02-11 08:13:44 +0000

    update processor

--- lerobot_teleoperator_pika/src/lerobot_teleoperator_pika/__init__.py
+++ lerobot_teleoperator_pika/src/lerobot_teleoperator_pika/__init__.py
@@ -2,9 +2,15 @@
 
 from .config_pika_teleoperator import PikaTeleoperatorConfig
 from .pika_teleoperator import PikaTeleoperator
+from .pika_to_piper_endpose_processor import MapPikaActionToPiperEndPose
 from .pika_to_piper_processor import MapPikaActionToPiperJoints
 
-__all__ = ["PikaTeleoperatorConfig", "PikaTeleoperator", "MapPikaActionToPiperJoints"]
+__all__ = [
+    "PikaTeleoperatorConfig",
+    "PikaTeleoperator",
+    "MapPikaActionToPiperJoints",
+    "MapPikaActionToPiperEndPose",
+]
 
 
 def main() -> None:
