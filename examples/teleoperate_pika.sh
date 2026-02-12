dd708b4da063bbc0a7b9df93f30dfe8200a3bd54
richardSHkim <shkim@superb-ai.com>
2026-02-12 12:54:42 +0000

    rename

--- examples/teleoperate_pika.sh
+++ examples/teleoperate_pika.sh
@@ -2,7 +2,7 @@
 
 python scripts/teleoperate_pika_with_calib.py \
   --calib calib.json \
-  --can can_follower \
+  --can can0 \
   --pika-port /dev/ttyUSB0 \
   --pika-device-key WM0 \
   --speed-ratio 70 \
